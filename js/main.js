85097f0ba1e3b6fc330c97dad826b13b95fecbfb
Alexandr Derkach <easysholder@gmail.com>
2018-08-18 05:01:46 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,387 @@
+TestPageCommon =
+{
+	CURRENCY_USD: 'USD',
+	CURRENCY_RUB: 'RUB',
+	CURRENCY_EUR: 'EUR',
+	CURRENCY_GBP: 'GBP',
+
+	CURRENCY_USD_UNICODE: '&#36;',
+	CURRENCY_RUB_UNICODE: '&#8381',
+	CURRENCY_EUR_UNICODE: '&#128;',
+	CURRENCY_GBP_UNICODE: '&#163;',
+
+	getSelectChangeContainer: function(obj)
+	{
+		return $(obj).closest('.js-exchange-select');
+	},
+
+	getExchangeInnerContainer: function(obj)
+	{
+		return $(obj).closest('.js-exchange-inner');
+	},
+
+	selectExchangeItem: function(obj)
+	{
+		var el = $(obj);
+		var container = TestPageCommon.getSelectChangeContainer(obj);
+		var elLiWrap = el.closest('li');
+		var elWrapSiblings = elLiWrap.siblings('.js-select-list-item');
+		var elId = container.find('.js-selected-item-id');
+		var currentItem = container.find('.js-exchange-select-item');
+		var list = container.find('.js-select-list');
+
+		list.hide();
+
+		if(!elLiWrap.hasClass('list-item-active'))
+		{
+			elLiWrap.addClass('list-item-active');
+		}
+
+		elWrapSiblings.removeClass('list-item-active');
+
+		if(elId.val())
+		{
+			elId.val('')
+		}
+
+		elId.val(el.html());
+
+		if(currentItem.html())
+		{
+			currentItem.html('')
+		}
+
+		currentItem.html(el.html());
+
+		TestPageCommon.getExchangeRequestData(el, elId.val());
+	},
+
+	openDropExchangeList: function(obj)
+	{
+		var container = TestPageCommon.getSelectChangeContainer(obj);
+		var list = container.find('.js-select-list');
+
+		if(list.is(':visible'))
+		{
+			list.hide();
+		}
+		else
+		{
+			list.show();
+		}
+	},
+
+	showCurrentItemOnReady: function()
+	{
+		var activeItem = $('.list-item-active');
+		var currentItem = $('.js-exchange-select-item');
+		var currentItemVal = activeItem.find('a').html();
+		var elId = $('.js-selected-item-id');
+
+		if(activeItem)
+		{
+			elId.val(currentItemVal);
+			currentItem.html(currentItemVal);
+		}
+	},
+
+	initSwitchClickWorking: function(obj)
+	{
+		var el = $(obj);
+		var slider = el.find('.js-exchange-slider');
+		var container = TestPageCommon.getExchangeInnerContainer(obj);
+		var currency = container.find('.js-selected-item-id').val();
+		var currentBlock = el.closest('.js-exchange-inner-block');
+		var isPercent = null;
+
+
+		if(!slider.hasClass('switch-checked'))
+		{
+			slider.addClass('switch-checked');
+			isPercent = true;
+		}
+		else
+		{
+			slider.removeClass('switch-checked');
+			isPercent = false;
+		}
+
+		TestPageCommon.getExchangeRequestData(obj, currency);
+		TestPageCommon.percentPointsManipulation(currentBlock, isPercent, currency);
+
+	},
+
+	getExchangeRequestData: function(obj, currency)
+	{
+		var container = TestPageCommon.getExchangeInnerContainer(obj);
+
+		TestPageCommon.getBitcoinRequestData(container, currency);
+		TestPageCommon.getEthereumRequestData(container, currency);
+		TestPageCommon.getLiteRequestData(container, currency);
+	},
+
+	changeCurrencyShortNameByRequestResult: function(currency, currentBlock, isChangeTypePoint)
+	{
+		var currName = null;
+
+		if(isChangeTypePoint)
+		{
+			currName = currentBlock.find('.js-short-type-points-name');
+		}
+		else
+		{
+			currName = currentBlock.find('.js-short-currency-name');
+		}
+
+		switch(currency)
+		{
+			case TestPageCommon.CURRENCY_USD:
+				currName.html(TestPageCommon.CURRENCY_USD_UNICODE);
+			break;
+
+			case TestPageCommon.CURRENCY_RUB:
+				currName.html(TestPageCommon.CURRENCY_RUB_UNICODE);
+			break;
+
+			case TestPageCommon.CURRENCY_EUR:
+				currName.html(TestPageCommon.CURRENCY_EUR_UNICODE);
+			break;
+
+			case TestPageCommon.CURRENCY_GBP:
+				currName.html(TestPageCommon.CURRENCY_GBP_UNICODE);
+			break;
+		}
+	},
+
+	percentPointsManipulation: function(currentBlock, isPercent, currency)
+	{
+		var pointName = currentBlock.find('.js-short-type-points-name');
+		console.log('currentBlock 1.3', currentBlock);
+		if(isPercent)
+		{
+			pointName.html('%');
+			console.log('isPercent 1.0', isPercent);
+		}
+		else
+		{
+			TestPageCommon.changeCurrencyShortNameByRequestResult(currency, currentBlock, true);
+			console.log('isPercent 1.1', isPercent);
+		}
+	},
+
+	getBitcoinRequestData: function(container, currency)
+	{
+		var currentBlock = container.find('.js-bitcoin-block');
+
+		TestPageCommon.requestToApi(currentBlock, currency);
+	},
+
+	getEthereumRequestData: function(container, currency)
+	{
+		var currentBlock = container.find('.js-ethereum-block');
+
+		TestPageCommon.requestToApi(currentBlock, currency);
+	},
+
+	getLiteRequestData: function(container, currency)
+	{
+		var currentBlock = container.find('.js-litecoin-block');
+
+		TestPageCommon.requestToApi(currentBlock, currency);
+	},
+
+	requestToApi: function(currentBlock, currency)
+	{
+		var url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC' + currency;
+		var switchCondition = currentBlock.find('.js-exchange-slider').hasClass('switch-checked');
+
+		var exchangeDynamicFields =
+		{
+			price: currentBlock.find('.js-price--value'),
+			hourChanges: currentBlock.find('.js-hour-changes-value'),
+			dayChanges: currentBlock.find('.js-day-changes-value'),
+			weekChanges: currentBlock.find('.js-week-changes-value'),
+			monthChanges: currentBlock.find('.js-month-changes-value')
+		};
+
+		var currencyInfo =
+		{
+			last: null,
+			hour: null,
+			day: null,
+			week: null,
+			month: null
+		};
+
+		TestPageCommon.requestBody(url, switchCondition, exchangeDynamicFields, currencyInfo);
+		TestPageCommon.percentPointsManipulation(currentBlock, switchCondition, currency);
+		TestPageCommon.changeCurrencyShortNameByRequestResult(currency, currentBlock, false);
+	},
+
+	requestBody: function(url, switchCondition, exchangeDynamicFields, currencyInfo)
+	{
+		$.ajax({
+			url: url,
+			dataType: 'json',
+			success: function(results)
+			{
+				if(switchCondition)
+				{
+					TestPageCommon.getPercentValues(currencyInfo, results);
+				}
+				else
+				{
+					TestPageCommon.getNumbersValues(currencyInfo, results);
+				}
+
+				currencyInfo.last = results.last;
+
+			},
+			complete: function()
+			{
+				TestPageCommon.fillDynamecExchangeFields(exchangeDynamicFields, currencyInfo);
+
+				var dynamicElArr =
+					[
+						exchangeDynamicFields.price,
+						exchangeDynamicFields.hourChanges,
+						exchangeDynamicFields.dayChanges,
+						exchangeDynamicFields.weekChanges,
+						exchangeDynamicFields.monthChanges
+					];
+
+				TestPageCommon.checkForNegativeVal(dynamicElArr);
+			}
+		});
+	},
+
+	fillDynamecExchangeFields: function(exchangeDynamicFields, currencyInfo)
+	{
+		exchangeDynamicFields.price.html(currencyInfo.last);
+		exchangeDynamicFields.hourChanges.html(currencyInfo.hour);
+		exchangeDynamicFields.dayChanges.html(currencyInfo.day);
+		exchangeDynamicFields.weekChanges.html(currencyInfo.week);
+		exchangeDynamicFields.monthChanges.html(currencyInfo.month);
+	},
+
+	checkForNegativeVal: function(dynamicElArr)
+	{
+		for(var i = 0; i < dynamicElArr.length; i++)
+		{
+			if(dynamicElArr[i].html() < 0)
+			{
+				dynamicElArr[i].closest('div').addClass('red');
+			}
+			else
+			{
+				dynamicElArr[i].closest('div').removeClass('red');
+			}
+		}
+	},
+
+	getPercentValues: function(currencyInfo, results)
+	{
+		currencyInfo.hour = results.changes.percent.hour;
+		currencyInfo.day = results.changes.percent.day;
+		currencyInfo.week = results.changes.percent.week;
+		currencyInfo.month = results.changes.percent.month;
+	},
+
+	getNumbersValues: function(currencyInfo, results)
+	{
+		currencyInfo.hour = results.changes.price.hour;
+		currencyInfo.day = results.changes.price.day;
+		currencyInfo.week = results.changes.price.week;
+		currencyInfo.month = results.changes.price.month;
+	}
+};
+
+TestPageSupOnReady =
+{
+	loadCurrencyInfo: function()
+	{
+		var currency = $('.js-selected-item-id').val();
+		TestPageSupOnReady.initSwitchOnLoadWorking();
+		TestPageSupOnReady.getExchangeRequestDataById(currency);
+	},
+
+	getExchangeRequestDataById: function(currency)
+	{
+		TestPageSupOnReady.getBitcoinRequestDataById(currency);
+		TestPageSupOnReady.getEthereumRequestDataById(currency);
+		TestPageSupOnReady.getLiteRequestDataById(currency);
+	},
+
+	getBitcoinRequestDataById: function(currency)
+	{
+		var currentBlock = $('#ethereumBlock');
+
+		TestPageCommon.requestToApi(currentBlock, currency);
+	},
+
+	getEthereumRequestDataById: function(currency)
+	{
+		var currentBlock = $('#litecoinBlock');
+
+		TestPageCommon.requestToApi(currentBlock, currency);
+	},
+
+	getLiteRequestDataById: function(currency)
+	{
+		var currentBlock = $('#bitcoinBlock');
+
+		TestPageCommon.requestToApi(currentBlock, currency);
+	},
+
+	initSwitchOnLoadWorking: function()
+	{
+		var container = $('#exchangeInner');
+		var currency = container.find('.js-selected-item-id').val();
+		var currentBlock = container.find('.js-exchange-inner-block');
+		var slider = currentBlock.find('.js-exchange-slider');
+		var isPercent = null;
+
+		for(var i = 0; i < currentBlock.length; i++)
+		{
+			if(!slider.eq(i).hasClass('switch-checked'))
+			{
+				slider.eq(i).addClass('switch-checked');
+				isPercent = true;
+			}
+			else
+			{
+				slider.eq(i).removeClass('switch-checked');
+				isPercent = false;
+			}
+
+			var pointName = currentBlock.eq(i).find('.js-short-type-points-name');
+
+			if(isPercent)
+			{
+				pointName.html('%');
+			}
+			else
+			{
+				TestPageCommon.changeCurrencyShortNameByRequestResult(currency, currentBlock.eq(i), true)
+			}
+		}
+
+		TestPageSupOnReady.getExchangeRequestDataById(currency);
+	}
+};
+
+function bindEvents()
+{
+	$(document).ready(
+		function()
+		{
+			TestPageCommon.showCurrentItemOnReady();
+		}
+	);
+	$(document).ready(
+		function()
+		{
+			TestPageSupOnReady.loadCurrencyInfo();
+		}
+	);
+}
